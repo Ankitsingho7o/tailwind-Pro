5e7144c2aaa3c90b798e092fc880a52ce4e7a9f7
Nishant Kumar Singh <nishant220902@gmail.com>
2022-09-09 16:43:43 +0530

    changed bg of testimonial cards and modifies vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,15 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         login: resolve(__dirname, "login.html"),
         register: resolve(__dirname, "register.html"),
+        jsfile: resolve(__dirname, "script.js")
       },
+      output: {
+				// Default
+				// dir: 'dist',
+				// With laravel: laravel-app/public/js
+				dir: 'dist',
+				entryFileNames: 'app-script.js'
+			}
     },
   },
 });
