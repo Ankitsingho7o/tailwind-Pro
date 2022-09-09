85c741bf5e81f84519572b80f17e8a0555914a1d
Nishant Kumar Singh <nishant220902@gmail.com>
2022-09-09 15:33:04 +0530

    added hamburger menu button and its styling and scripts

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ $(document).ready(function(){
             $('nav').removeClass("Sticky");
         }
     }); 
+
+});
+
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+btn.addEventListener('click', ()=>{
+    btn.classList.toggle('open');
+    nav.classList.toggle('flex');
+    nav.classList.toggle('hidden');
 });
